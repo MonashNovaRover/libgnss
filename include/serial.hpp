320359c481e805b492b120abade14a802e92a491
Terry Tian <97772315+tytia@users.noreply.github.com>
2026-05-11 03:31:14 +1000

    SerialPort WIP

--- include/serial.hpp
+++ include/serial.hpp
@@ -8,6 +8,7 @@
 #include <cstdint>
 #include <string>
 #include <libserialport.h>
+#include <stdexcept>
 
 namespace libgnss
 {
@@ -22,16 +23,25 @@ public:
   SerialPort(const SerialPort&) = delete;
   SerialPort& operator=(const SerialPort&) = delete;
 
-  bool open(const std::string& device, int baud_rate);
+  void open(const std::string& port_name, int baud_rate);
   void close();
 
   [[nodiscard]] bool isOpen() const;
 
-  int read(uint8_t* buffer, size_t size);
-  int write(const uint8_t* buffer, size_t size);
+  int read(uint8_t* buffer, size_t count) const;
+  int write(const uint8_t* buffer, size_t count, unsigned int timeout_ms) const;
+
+  class SerialPortError : public std::runtime_error
+  {
+  public:
+    explicit SerialPortError(const std::string& msg)
+      : runtime_error(msg)
+    {
+    }
+  };
 
 private:
-  bool _open(const std::string& device, int baud_rate);
+  static int check(sp_return result);
 
   sp_port* port_;
 };
--- src/serial.cpp
+++ src/serial.cpp
@@ -4,6 +4,9 @@
 
 #include "serial.hpp"
 
+#include <string>
+#include <iostream>
+
 namespace libgnss
 {
 
@@ -17,51 +20,72 @@ SerialPort::~SerialPort()
   close();
 }
 
-bool SerialPort::open(const std::string& device, int baud_rate)
+void SerialPort::open(const std::string& port_name, const int baud_rate)
 {
   close();
-  if (!_open(device, baud_rate))
-  {
-    close();
-    return false;
-  }
-  return true;
-}
-
-bool SerialPort::_open(const std::string& device, int baud_rate)
-{
-  sp_return ret = sp_get_port_by_name(device.c_str(), &port_);
-  if (ret != SP_OK || port_ == nullptr)
-  {
-    return false;
-  }
-
-  ret = sp_open(port_, SP_MODE_READ_WRITE);
-  if (ret != SP_OK)
-  {
-    return false;
-  }
-
-  if (
-    sp_set_baudrate(port_, baud_rate) != SP_OK ||
-    sp_set_bits(port_, 8) != SP_OK ||
-    sp_set_parity(port_, SP_PARITY_NONE) != SP_OK ||
-    sp_set_stopbits(port_, 1) != SP_OK ||
-    sp_set_flowcontrol(port_, SP_FLOWCONTROL_NONE) != SP_OK)
-  {
-    return false;
-  }
-
-  return true;
+  check(sp_get_port_by_name(port_name.c_str(), &port_));
+  // configure port
+  check(sp_open(port_, SP_MODE_READ_WRITE));
+  check(sp_set_baudrate(port_, baud_rate));
+  check(sp_set_bits(port_, 8));
+  check(sp_set_parity(port_, SP_PARITY_NONE));
+  check(sp_set_stopbits(port_, 1));
+  check(sp_set_flowcontrol(port_, SP_FLOWCONTROL_NONE));
 }
 
 void SerialPort::close()
 {
-  if (port_ != nullptr)
+  try
   {
-    sp_close(port_);
-    sp_free_port(port_);
-    port_ = nullptr;
+    if (isOpen())
+    {
+      check(sp_close(port_));
+      sp_free_port(port_);
+      port_ = nullptr;
+    }
+  }
+  catch (const SerialPortError& e)
+  {
+    std::cerr << "Error closing serial port: " << e.what() << std::endl;
+  }
+}
+
+bool SerialPort::isOpen() const
+{
+  return port_ != nullptr;
+}
+
+int SerialPort::read(uint8_t* buffer, const size_t count) const
+{
+  return check(sp_blocking_read_next(port_, buffer, count, 0));
+}
+
+int SerialPort::write(
+  const uint8_t* buffer, const size_t count, const unsigned int timeout_ms) const
+{
+  return check(sp_blocking_write(port_, buffer, count, timeout_ms));
+}
+
+int SerialPort::check(const sp_return result)
+{
+  switch (result)
+  {
+    case SP_ERR_ARG:
+      throw SerialPortError("libserialport: invalid argument");
+    case SP_ERR_FAIL:
+    {
+      char* error_message = sp_last_error_message();
+      const std::string details = error_message != nullptr ? error_message : "unknown error";
+      sp_free_error_message(error_message);
+      throw SerialPortError("libserialport: operation failed: " + details);
+    }
+    case SP_ERR_SUPP:
+      throw SerialPortError("libserialport: operation not supported");
+    case SP_ERR_MEM:
+      throw SerialPortError("libserialport: couldn't allocate memory");
+    case SP_OK:
+    default:
+      return result;
   }
 }
 
